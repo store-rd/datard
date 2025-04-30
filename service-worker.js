780025af7d2748d8b5b826b3cdfdab9ad24dd055
store-rd <94608739+store-rd@users.noreply.github.com>
2025-04-30 20:10:54 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 // service-worker.js (الكود الأصلي مع التخزين المؤقت)
 
 // !!! هام: غيّر رقم الإصدار في كل مرة تحدث فيها أي من الملفات في urlsToCache !!!
-const CACHE_NAME = 'sales-inventory-v10.3-cache-v7'; // <--- تم التغيير إلى v5
+const CACHE_NAME = 'sales-inventory-v10.3-cache-v10'; // <--- تم التغيير إلى v5
 
 const urlsToCache = [
   './',                     // الجذر النسبي
